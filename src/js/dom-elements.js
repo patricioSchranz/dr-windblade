f889bbcd437e62a3bf72802e259ab766eac1e735
Patricio Schranz <patricio-schranz@hotmail.com>
2023-02-09 20:14:53 +0100

    workin on the hamburger menu

---
+++ src/js/dom-elements.js
@@ -0,0 +1,6 @@
+// -----------------------------
+// VARIABLES 4 DOM ELEMENTS
+
+const
+    hamburgerButton = document.querySelector('.hamburger_button'),
+    nav = document.querySelector('.header_nav')
---
+++ src/js/hamburger.js
@@ -0,0 +1,15 @@
+
+
+
+hamburgerButton.addEventListener('click', ()=>{
+
+    hamburgerButton.classList.toggle('hamburger-active')
+    nav.classList.toggle('nav-in')
+
+    // if(!hamburgerButton.classList.contains('hamburger-active')){
+    //     nav.toggle('nav-in')
+    // }
+    // else{
+    //     hamburgerButton.classList.remove('hamburger-active')
+    // }
+})
