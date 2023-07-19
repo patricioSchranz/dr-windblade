d8b1dfafc236c3fc74011240ac4acb8367dcf12e
Patricio Schranz <patricio-schranz@hotmail.com>
2023-07-19 21:06:19 +0200

    working on physical therapy

--- docs/physical-therapy.php
+++ docs/physical-therapy.php
@@ -5,43 +5,145 @@ $page = 'physical-therapy';
 require __DIR__ . './../views/header.php'
 ?>
 
-<!-- HEADING -->
-<div class="physical-therapy_heading">
-    <div class="physical-therapy_heading_img"></div>
-    <h2 class="physical-therapy_heading_headline">Physiotherapie</h2>
-</div>
+<!-- PHYSICAL THERAPIE -->
+<section class="physical-therapy">
+
+    <!-- HEADING -->
+    <header class="physical-therapy_heading">
+        <img class="physical-therapy_heading_img" src="../graphics/pics/yoga-16.jpg" alt="physical-therapy">
+        <h2 class="physical-therapy_heading_headline">Physiotherapie</h2>
+    </header>
 
 
-<!-- SHORT DESCRIPTION -->
-<div class="physical-therapy_row_short-description">
+    <!-- SHORT DESCRIPTION -->
+    <section class="physical-therapy_row short-description">
+        <h3 class="hidden">Short description</h3>
+        <div class="description">
+            <p class="description_paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, saepe. Explicabo officia commodi expedita, nesciunt ullam quasi atque eaque saepe totam soluta possimus, quas hic dolorem recusandae rerum ad illo. Lorem ipsum dolor sit amet.
+            </p>
+            <p class="description_paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, saepe. Explicabo officia commodi expedita, nesciunt ullam quasi atque eaque saepe totam soluta possimus, quas hic dolorem recusandae rerum ad illo.
+            </p>
+        </div>
 
-    <div class="description">
-        <p class="description_paragraph">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, saepe. Explicabo officia commodi expedita, nesciunt ullam quasi atque eaque saepe totam soluta possimus, quas hic dolorem recusandae rerum ad illo. Lorem ipsum dolor sit amet.
-        </p>
-        <p class="description_paragraph">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, saepe. Explicabo officia commodi expedita, nesciunt ullam quasi atque eaque saepe totam soluta possimus, quas hic dolorem recusandae rerum ad illo.
-        </p>
-    </div>
-
-    <img src="../graphics/icons_n_co/physical-therapy.svg" alt="physical therapy logo">
-</div>
+        <img src="../graphics/icons_n_co/physical-therapy.svg" alt="physical therapy logo">
+    </section>
 
 
-<!-- PHYSICAL TRAINER --> 
-<div class="physical-therapy_row_trainer">
-    <img src="../graphics/pics/physical-therapist.jpg" alt="the physical therapist doctor cute">
+    <!-- PHYSICAL TRAINER --> 
+    <section class="physical-therapy_row trainer">
+        <h3 class="hidden">The trainer</h3>
+        <img src="../graphics/pics/physical-therapist.jpg" alt="the physical therapist doctor cute">
 
-    <div class="about">
-        <p class="about_paragraph">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, aspernatur omnis atque commodi, impedit neque praesentium beatae ullam velit, vero nam ratione dolor et illum libero rerum asperiores blanditiis dolores? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit eos adipisci, placeat veritatis eum blanditiis sint voluptatum nam quos id ducimus rem eaque voluptate perferendis corrupti facere reiciendis est consequatur.
-        </p>
+        <div class="about">
+            <p class="about_paragraph">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, aspernatur omnis atque commodi, impedit neque praesentium beatae ullam velit, vero nam ratione dolor et illum libero rerum asperiores blanditiis dolores? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit eos adipisci, placeat veritatis eum blanditiis sint voluptatum nam quos id ducimus rem eaque voluptate perferendis corrupti facere reiciendis est consequatur.
+            </p>
 
-        <p class="about_paragraph">
-            Eveniet dolor deleniti, numquam delectus tempora rerum consequuntur commodi libero vitae ipsa totam vero nemo reiciendis debitis ipsum quod sequi quis amet, quam voluptates pariatur quo nam nulla eius. Dolores! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint expedita porro doloribus aspernatur vel beatae recusandae, laboriosam tenetur voluptatum maiores debitis voluptas eum autem cupiditate excepturi iste. Reiciendis, numquam totam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat amet dolorum ullam?
-        </p>
-    </div>
-</div>
+            <p class="about_paragraph">
+                Eveniet dolor deleniti, numquam delectus tempora rerum consequuntur commodi libero vitae ipsa totam vero nemo reiciendis debitis ipsum quod sequi quis amet, quam voluptates pariatur quo nam nulla eius. Dolores! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint expedita porro doloribus aspernatur vel beatae recusandae, laboriosam tenetur voluptatum maiores debitis voluptas eum autem cupiditate excepturi iste. Reiciendis, numquam totam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat amet dolorum ullam?
+            </p>
+        </div>
+    </section>
 
+    <!-- SERVICES PT 1 -->
+    <section class="physical-therapy_row_services">
+        <h3 class="hidden">Services</h3>
+
+        <div class="container-content">
+            <p class="heading">Mein physiotherapeutisches Leistugsspektrum umfasst :</p>
+
+            <ul class="list">
+                <li>
+                    Konservative sowie prä- und postoperative Therapie im Bereich der Traumatologie/Unfallchirurgie und Orthopädie bei Schmerzen, Bewegungseinschränkungen und muskuläre Dysbalancen (Wirbelsäule und Gelenke)</li>
+                <li>
+                    Erkrankungen rheumatischer Art
+                </li>
+                <li>
+                    Neurologische Behandlung 
+                </li>
+                <li>
+                    Beckenbodenrehabilitation
+                </li>
+                <li>
+                    Faszientraining
+                </li>
+                <li>
+                    Stressbehandlung
+                </li>
+                <li>
+                    Lymphdrainage
+                </li>
+                <li>
+                    Hausbesuche
+                </li>
+            </ul>
+        </div>
+
+        <img src="../graphics/pics/trainings-tools.jpg" alt="Trainingsgegenstände">
+    </section>
+
+    <!-- SERVICES PT 2 -->
+    <section class="physical-therapy_row_services">
+        <h3 class="hidden">Advanced Services</h3>
+
+        <div class="container-content">
+            <p class="heading">Meine Zusatzausbildungen sind :</p>
+
+            <ul class="list">
+                <li>
+                    Mobilisation des Nervensystems
+                </li>
+                <li>
+                    Kinesio-Taping
+                </li>
+                <li>
+                    Beckenbodendysfunktionen
+                </li>
+                <li>
+                    Beckenboden und Kraft
+                </li>
+                <li>
+                    Wochenbett und Rückbildung
+                </li>
+                <li>
+                    Bandscheiben De- und Rehydratation
+                </li>
+                <li>
+                    Faszio – Das ganzheitliche Faszientraining
+                </li>
+                <li>
+                    Klinisches Patientenmanagement
+                </li>
+                <li>
+                    Orthomolekulare Medizin
+                </li>
+                <li>
+                    Hormonsystem
+                </li>
+                <li>
+                    Better Aging
+                </li>
+                <li>
+                    Behandlung bei Kopfschmerzen/Migräne
+                </li>
+                <li>
+                    Akupressur Schulter/Nacken
+                </li>
+                <li>
+                    Meniskuspathologie
+                </li>
+            </ul>
+        </div>
+
+        <img src="../graphics/pics/advanced-service.jpg" alt="eine Frau wird massiert">
+    </section>
+
+    <a href="tel:+4365030350589">
+        <span>Terminvereinbarungen unter 0650/30350589</span>
+    </a>
+
+</section><!-- END PHYSICAL THERAPY -->
 
 <?php require __DIR__ . './../views/footer.php' ?>
