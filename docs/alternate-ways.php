144f5398cbae6c16b47c216327719d6461e1807f
Patricio Schranz <patricio-schranz@hotmail.com>
2023-08-14 17:07:24 +0200

    working on alternate ways

--- docs/alternate-ways.php
+++ docs/alternate-ways.php
@@ -7,6 +7,7 @@ require __DIR__ . './../views/header.php';
 
 <!-- ALTERNATE WAYS --> 
 <section class="alternate-ways">
+    
     <!-- HEADER -->
     <header class="alternate-ways_header">
         <img src="../graphics/pics/alternate-med.jpg" alt="a chamomile tincture">
@@ -25,86 +26,121 @@ require __DIR__ . './../views/header.php';
 
         <!-- ACUPUNCTURE -->
         <article class="alternate-ways_tcm_topic">
+
             <header>  
-                <h4>Akkupunktur</h4>
+                <h4>
+                    <span class="heading-markup">Akkupunktur</span>
+                </h4>
                 <img src="../graphics/pics/acupuncture.jpg" alt="acupuncture">
             </header>
             
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, esse minus aliquid quaerat molestiae beatae dolores. Eius id consequuntur, nesciunt temporibus, praesentium cum itaque voluptas saepe maxime libero architecto voluptatibus.</p>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus praesentium, numquam velit corrupti id, accusantium iusto, nulla repellat cumque voluptas eveniet excepturi assumenda! Totam, explicabo. Debitis nulla quam fugiat eaque?
-            Corporis error voluptates ipsa nam impedit eius itaque quaerat quibusdam cumque velit sed repudiandae molestias quis sit hic ipsam, est amet quae assumenda officia? Perspiciatis rerum est dolore ratione magni?
-            Dolores commodi dolore, doloribus iusto fugiat aut nobis laboriosam similique odit esse sapiente, aliquid neque? Excepturi est voluptatem exercitationem ratione impedit in, rem fugit vitae quia quaerat illum laborum itaque.</p>
-
-           <a href="./alternate-services.php/#acupuncture">Zu meinem Akkupunktur Angebot</a>
+            <div class="container-description">
+                <div class="on-the-point">
+                    <p><strong>Lorem ipsum dolor sit, amet consectetur</strong> adipisicing elit. Impedit, esse minus aliquid quaerat molestiae beatae dolores. Eius id consequuntur, nesciunt temporibus, praesentium cum itaque <strong>voluptas saepe</strong> maxime libero architecto voluptatibus.</p>
+                </div>
+            
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus praesentium, numquam velit corrupti id, accusantium iusto, nulla <strong>repellat cumque voluptas eveniet</strong> sexcepturi assumenda! Totam, explicabo. Debitis nulla quam fugiat eaque?
+                Corporis error voluptates ipsa nam impedit eius itaque quaerat quibusdam cumque velit sed repudiandae molestias quis sit hic ipsam, est amet quae assumenda officia?<strong>Perspiciatis rerum est dolore</strong> ratione magni?
+                Dolores commodi dolore, doloribus iusto fugiat aut nobis laboriosam similique odit esse sapiente, aliquid neque? Excepturi est voluptatem <strong>exercitationem</strong> ratione impedit in, rem fugit vitae quia quaerat illum laborum itaque.</p>
+            </div>
+     
+           <a href="./alternate-services.php#acupuncture">Zu meinem Akkupunktur Angebot</a>
         </article>
 
         <!-- TUINA -->
         <article class="alternate-ways_tcm_topic">
 
             <header>
-                <h4>Tuina</h4>
+                <h4>
+                    <span class="heading-markup">Tuina</span>
+                </h4>
                 <img src="../graphics/pics/massage.jpg" alt="massging hands in action">
             </header>
             
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, esse minus aliquid quaerat molestiae beatae dolores. Eius id consequuntur, nesciunt temporibus, praesentium cum itaque voluptas saepe maxime libero architecto voluptatibus.</p>
+            <div class="container-description">
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus praesentium, numquam velit corrupti id, accusantium iusto, nulla repellat cumque voluptas eveniet excepturi assumenda! Totam, explicabo. Debitis nulla quam fugiat eaque?
-            Corporis error voluptates ipsa nam impedit eius itaque quaerat quibusdam cumque velit sed repudiandae molestias quis sit hic ipsam, est amet quae assumenda officia? Perspiciatis rerum est dolore ratione magni?
-            Dolores commodi dolore, doloribus iusto fugiat aut nobis laboriosam similique odit esse sapiente, aliquid neque? Excepturi est voluptatem exercitationem ratione impedit in, rem fugit vitae quia quaerat illum laborum itaque.</p>
+                <div class="on-the-point">
+                    <p><strong>Lorem ipsum dolor sit, amet consectetur</strong> adipisicing elit. Impedit, esse minus aliquid quaerat molestiae beatae dolores. Eius id consequuntur, nesciunt temporibus, praesentium cum itaque <strong>voluptas saepe</strong> maxime libero architecto voluptatibus.</p>
+                </div>
 
-           <a href="./alternate-services.php/#tuina">Zu meinem Tuina Angebot</a>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus praesentium, numquam velit corrupti id, accusantium iusto, nulla <strong>repellat cumque voluptas eveniet</strong> sexcepturi assumenda! Totam, explicabo. Debitis nulla quam fugiat eaque?
+                Corporis error voluptates ipsa nam impedit eius itaque quaerat quibusdam cumque velit sed repudiandae molestias quis sit hic ipsam, est amet quae assumenda officia?<strong>Perspiciatis rerum est dolore</strong> ratione magni?
+                Dolores commodi dolore, doloribus iusto fugiat aut nobis laboriosam similique odit esse sapiente, aliquid neque? Excepturi est voluptatem <strong>exercitationem</strong> ratione impedit in, rem fugit vitae quia quaerat illum laborum itaque.</p>
+            </div>
+
+           <a href="./alternate-services.php#tuina">Zu meinem Tuina Angebot</a>
         </article>
 
         <!-- MOXIBUSTION -->
         <article class="alternate-ways_tcm_topic">
             
             <header>
-                <h4>Moxibustion</h4>
+                <h4>
+                    <span class="heading-markup">Moxibustion</span>
+                </h4>
                 <img src="../graphics/pics/moxibustion.jpg" alt="moxibustion">
             </header>
             
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, esse minus aliquid quaerat molestiae beatae dolores. Eius id consequuntur, nesciunt temporibus, praesentium cum itaque voluptas saepe maxime libero architecto voluptatibus.</p>
+            <div class="container-description">
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus praesentium, numquam velit corrupti id, accusantium iusto, nulla repellat cumque voluptas eveniet excepturi assumenda! Totam, explicabo. Debitis nulla quam fugiat eaque?
-            Corporis error voluptates ipsa nam impedit eius itaque quaerat quibusdam cumque velit sed repudiandae molestias quis sit hic ipsam, est amet quae assumenda officia? Perspiciatis rerum est dolore ratione magni?
-            Dolores commodi dolore, doloribus iusto fugiat aut nobis laboriosam similique odit esse sapiente, aliquid neque? Excepturi est voluptatem exercitationem ratione impedit in, rem fugit vitae quia quaerat illum laborum itaque.</p>
+                <div class="on-the-point">
+                    <p><strong>Lorem ipsum dolor sit, amet consectetur</strong> adipisicing elit. Impedit, esse minus aliquid quaerat molestiae beatae dolores. Eius id consequuntur, nesciunt temporibus, praesentium cum itaque <strong>voluptas saepe</strong> maxime libero architecto voluptatibus.</p>
+                </div>
 
-           <a href="./alternate-services.php/#tuina">Zu meinem Moxibustion Angebot</a>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus praesentium, numquam velit corrupti id, accusantium iusto, nulla <strong>repellat cumque voluptas eveniet</strong> sexcepturi assumenda! Totam, explicabo. Debitis nulla quam fugiat eaque?
+                Corporis error voluptates ipsa nam impedit eius itaque quaerat quibusdam cumque velit sed repudiandae molestias quis sit hic ipsam, est amet quae assumenda officia?<strong>Perspiciatis rerum est dolore</strong> ratione magni?
+                Dolores commodi dolore, doloribus iusto fugiat aut nobis laboriosam similique odit esse sapiente, aliquid neque? Excepturi est voluptatem <strong>exercitationem</strong> ratione impedit in, rem fugit vitae quia quaerat illum laborum itaque.</p>
+            </div>
+
+           <a href="./alternate-services.php#tuina">Zu meinem Moxibustion Angebot</a>
         </article>
 
         <!-- NUTRITIONAL ADVICE -->
         <article class="alternate-ways_tcm_topic">
 
             <header>
-                <h4>Ernährungsberatung</h4>
+                <h4>
+                    <span class="heading-markup">Ernährungsberatung</span>
+                </h4>
                 <img src="../graphics/pics/nutritional.jpg" alt="nutritional">
             </header>
             
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, esse minus aliquid quaerat molestiae beatae dolores. Eius id consequuntur, nesciunt temporibus, praesentium cum itaque voluptas saepe maxime libero architecto voluptatibus.</p>
+            <div class="container-description">
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus praesentium, numquam velit corrupti id, accusantium iusto, nulla repellat cumque voluptas eveniet excepturi assumenda! Totam, explicabo. Debitis nulla quam fugiat eaque?
-            Corporis error voluptates ipsa nam impedit eius itaque quaerat quibusdam cumque velit sed repudiandae molestias quis sit hic ipsam, est amet quae assumenda officia? Perspiciatis rerum est dolore ratione magni?
-            Dolores commodi dolore, doloribus iusto fugiat aut nobis laboriosam similique odit esse sapiente, aliquid neque? Excepturi est voluptatem exercitationem ratione impedit in, rem fugit vitae quia quaerat illum laborum itaque.</p>
+                <div class="on-the-point">
+                    <p><strong>Lorem ipsum dolor sit, amet consectetur</strong> adipisicing elit. Impedit, esse minus aliquid quaerat molestiae beatae dolores. Eius id consequuntur, nesciunt temporibus, praesentium cum itaque <strong>voluptas saepe</strong> maxime libero architecto voluptatibus.</p>
+                </div>
 
-           <a href="./alternate-services.php/#nutritional-advice">Zu meinem Ernährungsberatungs Angebot</a>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus praesentium, numquam velit corrupti id, accusantium iusto, nulla <strong>repellat cumque voluptas eveniet</strong> sexcepturi assumenda! Totam, explicabo. Debitis nulla quam fugiat eaque?
+                Corporis error voluptates ipsa nam impedit eius itaque quaerat quibusdam cumque velit sed repudiandae molestias quis sit hic ipsam, est amet quae assumenda officia?<strong>Perspiciatis rerum est dolore</strong> ratione magni?
+                Dolores commodi dolore, doloribus iusto fugiat aut nobis laboriosam similique odit esse sapiente, aliquid neque? Excepturi est voluptatem <strong>exercitationem</strong> ratione impedit in, rem fugit vitae quia quaerat illum laborum itaque.</p>
+            </div>
+
+           <a href="./alternate-services.php#nutritional-advice">Zu meinem Ernährungsberatungs Angebot</a>
         </article>
 
         <!-- QI GONG -->
         <article class="alternate-ways_tcm_topic">
 
             <header>
-                <h4>Qi Gong</h4>
+                <h4>
+                    <span class="heading-markup">Qi Gong</span>
+                </h4>
                 <img src="../graphics/pics/qi-gong.jpg" alt="qi gong">
             </header>
             
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, esse minus aliquid quaerat molestiae beatae dolores. Eius id consequuntur, nesciunt temporibus, praesentium cum itaque voluptas saepe maxime libero architecto voluptatibus.</p>
+            <div class="container-description">
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus praesentium, numquam velit corrupti id, accusantium iusto, nulla repellat cumque voluptas eveniet excepturi assumenda! Totam, explicabo. Debitis nulla quam fugiat eaque?
-            Corporis error voluptates ipsa nam impedit eius itaque quaerat quibusdam cumque velit sed repudiandae molestias quis sit hic ipsam, est amet quae assumenda officia? Perspiciatis rerum est dolore ratione magni?
-            Dolores commodi dolore, doloribus iusto fugiat aut nobis laboriosam similique odit esse sapiente, aliquid neque? Excepturi est voluptatem exercitationem ratione impedit in, rem fugit vitae quia quaerat illum laborum itaque.</p>
+                <div class="on-the-point">
+                    <p><strong>Lorem ipsum dolor sit, amet consectetur</strong> adipisicing elit. Impedit, esse minus aliquid quaerat molestiae beatae dolores. Eius id consequuntur, nesciunt temporibus, praesentium cum itaque <strong>voluptas saepe</strong> maxime libero architecto voluptatibus.</p>
+                </div>
 
-           <a href="./alternate-services.php/#qi-gong">Zu meinem Qi Gong Angebot</a>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus praesentium, numquam velit corrupti id, accusantium iusto, nulla <strong>repellat cumque voluptas eveniet</strong> sexcepturi assumenda! Totam, explicabo. Debitis nulla quam fugiat eaque?
+                Corporis error voluptates ipsa nam impedit eius itaque quaerat quibusdam cumque velit sed repudiandae molestias quis sit hic ipsam, est amet quae assumenda officia?<strong>Perspiciatis rerum est dolore</strong> ratione magni?
+                Dolores commodi dolore, doloribus iusto fugiat aut nobis laboriosam similique odit esse sapiente, aliquid neque? Excepturi est voluptatem <strong>exercitationem</strong> ratione impedit in, rem fugit vitae quia quaerat illum laborum itaque.</p>
+            </div>
+
+           <a href="./alternate-services.php#qi-gong">Zu meinem Qi Gong Angebot</a>
         </article>
     </section>
 </section>
