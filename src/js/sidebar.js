759e4e2dafd68af86e68459c33941a48ca8880ea
Patricio Schranz <faguzz.media@gmail.com>
2023-12-11 20:13:47 +0100

    new filter button

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -6,7 +6,27 @@ const
     sidebarLinks = document.querySelectorAll('.blog_sidebar a'),
     sidebarClose = document.querySelector(".sidebar-close"),
     sidebar = document.querySelector('.blog_sidebar'),
-    filterButton = document.querySelector('.blog-filter')
+    filterButton = document.querySelector('.blog-filter'),
+    windowHeight = window.innerHeight,
+    documentHeight = document.querySelector('body').offsetHeight,
+    footerHeight = document.querySelector('.footer').offsetHeight
+
+// => show the filter button
+document.addEventListener('scroll', ()=>{
+
+   if(scrollY > (windowHeight * 0.2) && scrollY < (documentHeight - footerHeight) ){
+    filterButton.classList.contains('visible-button') || filterButton.classList.add('visible-button')
+   }
+
+   if(scrollY < (windowHeight * 0.2) ){
+    filterButton.classList.contains('visible-button') && filterButton.classList.remove('visible-button')
+   }
+
+   if(scrollY >= (documentHeight - (footerHeight + windowHeight) ) ){
+    filterButton.classList.contains('visible-button') && filterButton.classList.remove('visible-button')
+   }
+
+})
 
 
 // => open the sidebar
@@ -23,4 +43,6 @@ sidebarClose.addEventListener('click', () =>{
 sidebarLinks.forEach(link => link.addEventListener('click', () => {
     sidebar.classList.toggle('visible-sidebar')
 }))
+
+
 
