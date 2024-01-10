8f4f3e5e4e79d651f6dd4de19d1da525319fbffe
Patricio Schranz <faguzz.media@gmail.com>
2024-01-10 20:29:18 +0100

    finished service animations

--- docs/corona-services.php
+++ docs/corona-services.php
@@ -32,34 +32,45 @@ require __DIR__ . './../views/header.php';
         <h2 class="outer-heading" data-id="#covid-antigen">Covid 19 Antigentest</h2>
 
         <div class="services_service_content">
-            <img src="../graphics/pics/antigen.jpg">
+
+            <figure class="
+                    animation-element
+                    animation--style_the-circle-grows
+                    animation--duration_1000
+                    animation--delay_0
+                    animation--timing_cubic-bezier(.47,.03,.68,.36)
+                    animation--trigger-point_70
+                    ">
+                <img src="../graphics/pics/antigen.jpg">
+            </figure>
 
             <div class="services_service_text">
-                <h2 class="inner-heading">Covid 19 Antigentest</h2>
+                <h2 class="animation-element animation--style_flipping-headline animation--delay_300">Covid 19 Antigentest</h2>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellendus deserunt minima iusto quibusdam ea ipsa quas aliquam dolores maxime, nobis molestias voluptates omnis adipisci animi ratione, vero ducimus recusandae.</p>
+                <p class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellendus deserunt minima iusto quibusdam ea ipsa quas aliquam dolores maxime, nobis molestias voluptates omnis adipisci animi ratione, vero ducimus recusandae.
+                 </p>
 
-                <h3>Lorem ipsum dolor sit amet.</h3>
+                <h3 class="animation-element animation--style_clippy-from-left-to-right animation--duration_1000 animation--trigger-point_90">Lorem ipsum dolor sit amet.</h3>
 
                 <ul>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
                 </ul>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem magni accusantium atque saepe. Dicta tenetur amet quo exercitationem itaque alias ab totam hic esse, recusandae quasi optio? Fugit, quisquam adipisci.
+                <p class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem magni accusantium atque saepe. Dicta tenetur amet quo exercitationem itaque alias ab totam hic esse, recusandae quasi optio? Fugit, quisquam adipisci.
                 Porro hic aut vel distinctio fugit, repellendus omnis adipisci in suscipit aliquam non dolorum eligendi aliquid, esse eaque doloribus ea et labore modi exercitationem quisquam! Voluptatum reprehenderit nam quidem fugiat!</p>
             </div>  
         </div>
@@ -72,34 +83,45 @@ require __DIR__ . './../views/header.php';
         <h2 class="outer-heading" data-id="#covid-pcr">Covid 19 PCR Test</h2>
 
         <div class="services_service_content">
-            <img src="../graphics/pics/pcr.jpg">
+
+            <figure class="
+                    animation-element
+                    animation--style_the-circle-grows
+                    animation--duration_1000
+                    animation--delay_0
+                    animation--timing_cubic-bezier(.47,.03,.68,.36)
+                    animation--trigger-point_70
+                    ">
+                <img src="../graphics/pics/pcr.jpg">
+            </figure>
 
             <div class="services_service_text">
-                <h2>Covid 19 PCR Tes</h2>
+                <h2 class="animation-element animation--style_flipping-headline animation--delay_300">Covid 19 PCR Tes</h2>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellendus deserunt minima iusto quibusdam ea ipsa quas aliquam dolores maxime, nobis molestias voluptates omnis adipisci animi ratione, vero ducimus recusandae.</p>
+                <p class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellendus deserunt minima iusto quibusdam ea ipsa quas aliquam dolores maxime, nobis molestias voluptates omnis adipisci animi ratione, vero ducimus recusandae.
+                 </p>
 
-                <h3>Lorem ipsum dolor sit amet.</h3>
+                <h3 class="animation-element animation--style_clippy-from-left-to-right animation--duration_1000 animation--trigger-point_90">Lorem ipsum dolor sit amet.</h3>
 
                 <ul>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
                 </ul>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem magni accusantium atque saepe. Dicta tenetur amet quo exercitationem itaque alias ab totam hic esse, recusandae quasi optio? Fugit, quisquam adipisci.
+                <p class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem magni accusantium atque saepe. Dicta tenetur amet quo exercitationem itaque alias ab totam hic esse, recusandae quasi optio? Fugit, quisquam adipisci.
                 Porro hic aut vel distinctio fugit, repellendus omnis adipisci in suscipit aliquam non dolorum eligendi aliquid, esse eaque doloribus ea et labore modi exercitationem quisquam! Voluptatum reprehenderit nam quidem fugiat!</p>
             </div>  
         </div>
@@ -112,34 +134,45 @@ require __DIR__ . './../views/header.php';
         <h2 class="outer-heading" data-id="#covid-accompaniment">Long Covid Begleitung</h2>
 
         <div class="services_service_content">
-            <img src="../graphics/pics/accompaniment.jpg">
+
+            <figure class="
+                    animation-element
+                    animation--style_the-circle-grows
+                    animation--duration_1000
+                    animation--delay_0
+                    animation--timing_cubic-bezier(.47,.03,.68,.36)
+                    animation--trigger-point_70
+                    ">
+                <img src="../graphics/pics/accompaniment.jpg">
+            </figure>
 
             <div class="services_service_text">
-                <h2>Long Covid Begleitung</h2>
+                <h2 class="animation-element animation--style_flipping-headline animation--delay_300">Long Covid Begleitung</h2>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellendus deserunt minima iusto quibusdam ea ipsa quas aliquam dolores maxime, nobis molestias voluptates omnis adipisci animi ratione, vero ducimus recusandae.</p>
+                <p class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellendus deserunt minima iusto quibusdam ea ipsa quas aliquam dolores maxime, nobis molestias voluptates omnis adipisci animi ratione, vero ducimus recusandae.
+                 </p>
 
-                <h3>Lorem ipsum dolor sit amet.</h3>
+                <h3 class="animation-element animation--style_clippy-from-left-to-right animation--duration_1000 animation--trigger-point_90">Lorem ipsum dolor sit amet.</h3>
 
                 <ul>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
-                    <li>
+                    <li class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">
                         Lorem, ipsum dolor.
                     </li>
                 </ul>
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem magni accusantium atque saepe. Dicta tenetur amet quo exercitationem itaque alias ab totam hic esse, recusandae quasi optio? Fugit, quisquam adipisci.
+                <p class="animation-element animation--style_fade-up animation--duration_500 animation--trigger-point_90">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem magni accusantium atque saepe. Dicta tenetur amet quo exercitationem itaque alias ab totam hic esse, recusandae quasi optio? Fugit, quisquam adipisci.
                 Porro hic aut vel distinctio fugit, repellendus omnis adipisci in suscipit aliquam non dolorum eligendi aliquid, esse eaque doloribus ea et labore modi exercitationem quisquam! Voluptatum reprehenderit nam quidem fugiat!</p>
             </div>  
         </div>
