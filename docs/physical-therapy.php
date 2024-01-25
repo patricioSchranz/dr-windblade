0b3edff48b72038f1f5f0c2c42541f0b4db4f00a
Patricio Schranz <faguzz.media@gmail.com>
2024-01-25 19:50:49 +0100

    add animations => physical therapy

--- docs/physical-therapy.php
+++ docs/physical-therapy.php
@@ -11,7 +11,7 @@ require __DIR__ . './../views/header.php'
     <!-- HEADING -->
     <header class="physical-therapy_heading">
         <img class="physical-therapy_heading_img" src="../graphics/pics/yoga-16.jpg" alt="physical-therapy">
-        <h2 class="physical-therapy_heading_headline">Physiotherapie</h2>
+        <h2 class="physical-therapy_heading_headline animation-element animation--style_clippy-circle animation--duration_500 animation--trigger-point_95 animation--delay_0">Physiotherapie</h2>
     </header>
 
 
@@ -19,24 +19,24 @@ require __DIR__ . './../views/header.php'
     <section class="physical-therapy_row short-description">
         <h3 class="hidden">Short description</h3>
         <div class="description">
-            <p class="description_paragraph">
+            <p class="description_paragraph animation-element animation--style_fade-from-left">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, saepe. Explicabo officia commodi expedita, nesciunt ullam quasi atque eaque saepe totam soluta possimus, quas hic dolorem recusandae rerum ad illo. Lorem ipsum dolor sit amet.
             </p>
-            <p class="description_paragraph">
+            <p class="description_paragraph animation-element animation--style_fade-up">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, saepe. Explicabo officia commodi expedita, nesciunt ullam quasi atque eaque saepe totam soluta possimus, quas hic dolorem recusandae rerum ad illo.
             </p>
         </div>
 
-        <img src="../graphics/icons_n_co/physical-therapy.svg" alt="physical therapy logo">
+        <img src="../graphics/icons_n_co/physical-therapy.svg" alt="physical therapy logo" class="animation-element animation--style_clippy-from-top-to-bottom animation--duration_1000">
     </section>
 
 
     <!-- PHYSICAL TRAINER --> 
     <section class="physical-therapy_row trainer">
         <h3 class="hidden">The trainer</h3>
-        <img src="../graphics/pics/physical-therapist.jpg" alt="the physical therapist doctor cute">
+        <img src="../graphics/pics/physical-therapist.jpg" alt="the physical therapist doctor cute" class="animation-element animation--style_clippy-circle animation--duration_1000">
 
-        <div class="about">
+        <div class="about animation-element animation--style_clippy-circle animation--duration_1000 animation--delay_200">
             <p class="about_paragraph">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, aspernatur omnis atque commodi, impedit neque praesentium beatae ullam velit, vero nam ratione dolor et illum libero rerum asperiores blanditiis dolores? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit eos adipisci, placeat veritatis eum blanditiis sint voluptatum nam quos id ducimus rem eaque voluptate perferendis corrupti facere reiciendis est consequatur.
             </p>
@@ -52,88 +52,88 @@ require __DIR__ . './../views/header.php'
         <h3 class="hidden">Services</h3>
 
         <div class="container-content">
-            <p class="heading">Mein physiotherapeutisches Leistugsspektrum umfasst :</p>
+            <p class="heading animation-element animation--style_clippy-from-left-to-right animation--duration_500">Mein physiotherapeutisches Leistugsspektrum umfasst :</p>
 
             <ul class="list">
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Konservative sowie prä- und postoperative Therapie im Bereich der Traumatologie/Unfallchirurgie und Orthopädie bei Schmerzen, Bewegungseinschränkungen und muskuläre Dysbalancen (Wirbelsäule und Gelenke)</li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Erkrankungen rheumatischer Art
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Neurologische Behandlung 
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Beckenbodenrehabilitation
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Faszientraining
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Stressbehandlung
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Lymphdrainage
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Hausbesuche
                 </li>
             </ul>
         </div>
 
-        <img src="../graphics/pics/trainings-tools.jpg" alt="Trainingsgegenstände">
+        <img src="../graphics/pics/trainings-tools.jpg" alt="Trainingsgegenstände" class="animation-element animation--style_opacity-in animation--duration_2000">
     </section>
 
     <!-- SERVICES PT 2 -->
     <section class="physical-therapy_row services">
         <h3 class="hidden">Advanced Services</h3>
 
-        <img src="../graphics/pics/advanced-service.jpg" alt="eine Frau wird massiert">
+        <img src="../graphics/pics/advanced-service.jpg" alt="eine Frau wird massiert" class="animation-element animation--style_opacity-in animation--duration_2000">
 
         <div class="container-content">
-            <p class="heading">Meine Zusatzausbildungen sind :</p>
+            <p class="heading animation-element animation--style_clippy-from-left-to-right animation--duration_500">Meine Zusatzausbildungen sind :</p>
 
             <ul class="list">
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Mobilisation des Nervensystems
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Kinesio-Taping
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Beckenbodendysfunktionen
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Beckenboden und Kraft
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Wochenbett und Rückbildung
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Bandscheiben De- und Rehydratation
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Faszio – Das ganzheitliche Faszientraining
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Klinisches Patientenmanagement
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Orthomolekulare Medizin
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Hormonsystem
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Better Aging
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Behandlung bei Kopfschmerzen/Migräne
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Akupressur Schulter/Nacken
                 </li>
-                <li>
+                <li class="animation-element animation--style_fade-up">
                     Meniskuspathologie
                 </li>
             </ul>
@@ -141,7 +141,7 @@ require __DIR__ . './../views/header.php'
 
     </section>
 
-    <a href="tel:+4365030350589" class="tel-button">
+    <a href="tel:+4365030350589" class="tel-button animation-element animation--style_flipping-headline animation--duration_1000 animation--delay_800">
         <span>Terminvereinbarungen unter 0650/30350589</span>
     </a>
 
