6e2f033c7bb49b5073d2d624ee7c87462f6d399d
Patricio Schranz <faguzz.media@gmail.com>
2024-03-18 20:54:43 +0100

    performance optimization: home

--- index.php
+++ index.php
@@ -26,60 +26,16 @@ require __DIR__ . '/views/header-index.php';
 
             <div class="startpage_leistungen_card-container">
 
-        <!-- CARD KASSENLEISTUNGEN -->
-            <!-- <div class="startpage_leistungen_card animate__animated" id="card-kassen">
-                <div class="startpage_leistungen_card_header">
-                    <img src="graphics/pics/stethoscope.webp" alt="stethoscope">
-                    <h3>Kassenleistungen</h3>
-                </div>
-
-                <ul class="startpage_leistungen_card_list">
-                    <li>
-                        <span>Vorsorgeuntersuchung</span>
-                    </li>
-                    <li>
-                        <span>Ruhe EKG</span>
-                    </li>
-                    <li>
-                        <span>Lungenfunktion</span>
-                    </li>
-                    <li>
-                        <span>uvm.</span>
-                    </li>
-                </ul>
-
-                <button class="startpage_leistungen_card_button" id="button_kassenleistungen">zu den Kassenleistungen</button>
-            </div> -->
-
-
-        <!-- CARD WAHLARZTLEISTUNGEN -->
-            <!-- <div class="startpage_leistungen_card animate__animated" id="card-wahlarzt">
-                <div class="startpage_leistungen_card_header">
-                    <img src="graphics/pics/analysis.webp" alt="stethoscope">
-                    <h3>Wahlarztleistungen</h3>
-                </div>
-
-                <ul class="startpage_leistungen_card_list">
-                    <li>
-                        <span>24h EKG</span>
-                    </li>
-                    <li>
-                        <span>24h Blutdruck</span>
-                    </li>
-                    <li>
-                        <span>Eventrecorder</span>
-                    </li>
-                    <li>
-                        <span>uvm.</span>
-                    </li>
-                </ul>
-
-                <button class="startpage_leistungen_card_button" id="button_wahlarztleistungen">zu den Wahlarztleistungen</button>
-            </div> -->
-
+   
              <!-- KASSENLEISTUNGEN (big card) -->
              <div class="startpage_leistungen_card big-card animate__animated" id="card-kassen">
-                <img src="graphics/pics/stethoscope.webp" alt="stethoscope">
+            
+                <picture>
+                    <source srcset="graphics/pics/stethoscope-750.webp" media="(max-width: 750px)">
+                    <source srcset="graphics/pics/stethoscope-1500.webp" media="(max-width: 1500px)">
+                    <source srcset="graphics/pics/stethoscope.webp" media="(min-width: 1500px)">
+                    <img src="graphics/pics/stethoscope.webp" alt="stethoscope" loading="lazy">
+                </picture>
 
                 <div class="big-card_card-container">
                     <div class="startpage_leistungen_card_header">
@@ -107,7 +63,13 @@ require __DIR__ . '/views/header-index.php';
 
             <!-- WAHLARZTLEISTUNGEN (big card) -->
             <div class="startpage_leistungen_card big-card animate__animated" id="card-wahlarzt">
-                <img src="graphics/pics/analysis.webp" alt="stethoscope">
+
+                <picture>
+                    <source srcset="graphics/pics/analysis-750.webp" media="(max-width: 750px)">
+                    <source srcset="graphics/pics/analysis-1500.webp" media="(max-width: 1500px)">
+                    <source srcset="graphics/pics/analysis.webp" media="(min-width: 1500px)">
+                    <img src="graphics/pics/analysis.webp" alt="analysis" loading="lazy">
+                </picture>
 
                 <div class="big-card_card-container">
                     <div class="startpage_leistungen_card_header">
@@ -136,7 +98,13 @@ require __DIR__ . '/views/header-index.php';
 
         <!-- CARD ALTERNATIVE MEDIZIN(big card) -->
             <div class="startpage_leistungen_card big-card animate__animated" id="card-aesthetic">
-                <img src="./graphics/pics/tea.webp" alt="woman" class="card-pic">
+
+                <picture>
+                    <source srcset="graphics/pics/tea-750.webp" media="(max-width: 750px)">
+                    <source srcset="graphics/pics/tea-1500.webp" media="(max-width: 1500px)">
+                    <source srcset="graphics/pics/tea.webp" media="(min-width: 1500px)">
+                    <img src="graphics/pics/tea.webp" alt="tea" loading="lazy">
+                </picture>
 
                 <div class="big-card_card-container">
                     <div class="startpage_leistungen_card_header">
@@ -164,7 +132,13 @@ require __DIR__ . '/views/header-index.php';
 
         <!-- CARD WEITERE LEISTUNGEN (big card) -->
             <div class="startpage_leistungen_card big-card animate__animated" id="card-more">
-                <img src="./graphics/pics/meditation.webp" alt="woman">
+
+                <picture>
+                    <source srcset="graphics/pics/meditation-750.webp" media="(max-width: 750px)">
+                    <source srcset="graphics/pics/meditation-1500.webp" media="(max-width: 1500px)">
+                    <source srcset="graphics/pics/meditation.webp" media="(min-width: 1500px)">
+                    <img src="graphics/pics/meditation.webp" alt="meditation" loading="lazy">
+                </picture>
 
                 <div class="big-card_card-container">
                     <div class="startpage_leistungen_card_header">
