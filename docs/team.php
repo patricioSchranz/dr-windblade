028bf06fe3c3b528a258f128f0d950234159f847
Patricio Schranz <faguzz.media@gmail.com>
2024-08-16 17:37:41 +0200

    performance optimization: team

--- docs/team.php
+++ docs/team.php
@@ -12,14 +12,31 @@ require __DIR__ . "/../views/header.php" ;
     <!-- HEADER -->
     <header class="team_header">
         <div class="left-side">
-            <img src="../graphics/pics/team-1.webp" alt="medical team" class="animation-element animation--style_windblade-img-duo-left animation--duration_1000">
-            <img src="../graphics/pics/team-2.webp" alt="medical team" class="animation-element animation--style_windblade-img-duo-right animation--duration_1000">
+
+            <picture class="animation-element animation--style_windblade-img-duo-left animation--duration_1000">
+                <source srcset="./../graphics/pics/team-1-wmc-750.webp" media="(max-width: 750px)">
+                <source srcset="./../graphics/pics/team-1-wmc-1500.webp" media="(max-width: 1500px)">
+                <source srcset="./../graphics/pics/team-1.webp" media="(min-width: 1500px)">
+                <img src="./../graphics/pics/team-1.webp" alt="medical team" loading="lazy">
+            </picture>
+
+            <picture class="animation-element animation--style_windblade-img-duo-right animation--duration_1000">
+                <source srcset="./../graphics/pics/team-2-wmc-750.webp" media="(max-width: 750px)">
+                <source srcset="./../graphics/pics/team-2-wmc-1500.webp" media="(max-width: 1500px)">
+                <source srcset="./../graphics/pics/team-2.webp" media="(min-width: 1500px)">
+                <img src="./../graphics/pics/team-2.webp" alt="medical team" loading="lazy">
+            </picture>
         </div>
 
         <div class="right-side">
             <h1>Unser Team</h1>
 
-            <img src="../graphics/pics/team-3.webp" alt="medical team" class="animation-element animation--style_opacity-in animation--duration_1000">
+            <picture class="animation-element animation--style_opacity-in animation--duration_1000">
+                <source srcset="./../graphics/pics/team-3-wmc-750.webp" media="(max-width: 750px)">
+                <source srcset="./../graphics/pics/team-3-wmc-1500.webp" media="(max-width: 1500px)">
+                <source srcset="./../graphics/pics/team-3.webp" media="(min-width: 1500px)">
+                <img src="./../graphics/pics/team-3.webp" alt="medical team" loading="lazy">
+            </picture>
         </div>
     </header>
 
@@ -29,7 +46,13 @@ require __DIR__ . "/../views/header.php" ;
 
         <!-- DR WINDBLADE -->
         <div class="team_members_member-container">
-            <img src="../graphics/pics/dr-winblade.webp" alt="dr-windblade" class="animation-element animation--style_trans-rotate-y-right-light animation--duration_500">
+
+            <picture class="animation-element animation--style_trans-rotate-y-right-light animation--duration_500">
+                <source srcset="./../graphics/pics/dr-winblade-wmc-750.webp" media="(max-width: 750px)">
+                <source srcset="./../graphics/pics/dr-winblade-wmc-1500.webp" media="(max-width: 1500px)">
+                <source srcset="./../graphics/pics/dr-winblade.webp" media="(min-width: 1500px)">
+                <img src="./../graphics/pics/dr-winblade.webp" alt="dr-windblade" loading="lazy">
+            </picture>
 
             <div class="text">
                 <h3>Dr. Windblade</h3>
@@ -42,7 +65,13 @@ require __DIR__ . "/../views/header.php" ;
 
         <!-- TAMARA ELVES -->
         <div class="team_members_member-container">
-            <img src="../graphics/pics/tamara-elves.webp" alt="tamara elves" class="animation-element animation--style_trans-rotate-y-left-light animation--duration_500">
+
+            <picture class="animation-element animation--style_trans-rotate-y-left-light animation--duration_500">
+                <source srcset="./../graphics/pics/tamara-elves-wmc-750.webp" media="(max-width: 750px)">
+                <source srcset="./../graphics/pics/tamara-elves-wmc-1500.webp" media="(max-width: 1500px)">
+                <source srcset="./../graphics/pics/tamara-elves.webp" media="(min-width: 1500px)">
+                <img src="./../graphics/pics/tamara-elves.webp" alt="tamara elves" loading="lazy">
+            </picture>
 
             <div class="text">
                 <h3>Tamara Elves</h3>
@@ -57,7 +86,13 @@ require __DIR__ . "/../views/header.php" ;
 
         <!-- SOFIA SKYBLUE -->
         <div class="team_members_member-container">
-            <img src="../graphics/pics/sofia-skyblue.webp" alt="sofia skyblue" class="animation-element animation--style_trans-rotate-y-right-light animation--duration_500">
+    
+            <picture class="animation-element animation--style_trans-rotate-y-right-light animation--duration_500">
+                <source srcset="./../graphics/pics/sofia-skyblue-wmc-750.webp" media="(max-width: 750px)">
+                <source srcset="./../graphics/pics/sofia-skyblue-wmc-1500.webp" media="(max-width: 1500px)">
+                <source srcset="./../graphics/pics/sofia-skyblue.webp" media="(min-width: 1500px)">
+                <img src="./../graphics/pics/sofia-skyblue.webp" alt="sofia skyblue" loading="lazy">
+            </picture>
 
             <div class="text">
                 <h3>Sofia Skyblue</h3>
@@ -72,7 +107,13 @@ require __DIR__ . "/../views/header.php" ;
 
         <!-- ANNA MARISSA -->
         <div class="team_members_member-container">
-            <img src="../graphics/pics/anna-marissa.webp" alt="anna marissa" class="animation-element animation--style_trans-rotate-y-left-light animation--duration_500">
+
+            <picture class="animation-element animation--style_trans-rotate-y-left-light animation--duration_500">
+                <source srcset="./../graphics/pics/anna-marissa-wmc-750.webp" media="(max-width: 750px)">
+                <source srcset="./../graphics/pics/anna-marissa-wmc-1500.webp" media="(max-width: 1500px)">
+                <source srcset="./../graphics/pics/anna-marissa.webp" media="(min-width: 1500px)">
+                <img src="./../graphics/pics/anna-marissa.webp" alt="anna marissa" loading="lazy">
+            </picture>
 
             <div class="text">
                 <h3>Anna Marissa</h3>
